ecd28153fa620972d98d648bce6bacca3283cbcd
Natalie <natalie@emeraldstay.com>
2020-09-21 17:03:17 +0200

    change db connection

--- db/db.js
+++ db/db.js
@@ -1,14 +1,17 @@
 'user strict'
 
-const mysql = require('mysql')
+const mysql = require('mysql');
+const { env } = require('process');
 require('dotenv').config()
 
-const connection = mysql.createConnection({
-  host     : 'gnldm1014.siteground.biz',
-  user     : 'umtnndmjmcezv',
-  password : process.env.DB_PASS,
-  database : 'db6hextktaa2yb',
-})
+// const connection = mysql.createConnection({
+//   host     : 'gnldm1014.siteground.biz',
+//   user     : 'umtnndmjmcezv',
+//   password : process.env.DB_PASS,
+//   database : 'db6hextktaa2yb',
+// })
+
+const connection = mysql.createConnection(`mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`);
 
 connection.connect((err) => {
   if (err) {
