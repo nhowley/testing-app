b40d7e4917edc273cf234df10271ab52a557a96c
Natalie <natalie@emeraldstay.com>
2020-09-21 19:26:44 +0200

    add database url

--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@
 const mysql = require('mysql');
 const { env } = require('process');
 require('dotenv').config()
+const { Pool, Client } = require('pg')
 
 // const connection = mysql.createConnection({
 //   host     : 'gnldm1014.siteground.biz',
@@ -11,14 +12,14 @@ require('dotenv').config()
 //   database : 'db6hextktaa2yb',
 // })
 
-const connection = mysql.createConnection(`mysql://${process.env.DB_USER}:${process.env.DB_PASS}@gnldm1014.siteground.biz/${process.env.DB_DATABASE}`);
+const connection = mysql.createConnection(`${process.env.DATABASE_URL}`);
 
 // const connection = new Pool({
-//   user: 'dbuser',
-//   host: 'database.server.com',
-//   database: 'mydb',
-//   password: 'secretpassword',
-//   port: 3211,
+//   user: 'wkjrrgatzzehmi',
+//   host: 'ec2-54-228-209-117.eu-west-1.compute.amazonaws.com',
+//   database: 'd98k2uh7gfp8f5',
+//   password: '0d1b97da4e0ae5fd9bd13327843717cfe1165cb29592817faf5fd1ef13d532f8',
+//   port: 5432,
 // })
 
 connection.connect((err) => {
