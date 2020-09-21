fe15bc6480884d26877556364df6cefa29c488ad
Natalie <natalie@emeraldstay.com>
2020-09-21 16:29:13 +0200

    remove local host db setup

--- db/db.js
+++ db/db.js
@@ -4,10 +4,10 @@ const mysql = require('mysql')
 require('dotenv').config()
 
 const connection = mysql.createConnection({
-  host     : process.env.DB_HOST || 'localhost',
-  user     : process.env.DB_USER || 'localhost',
-  password : process.env.DB_PASS || 'localhost',
-  database : process.env.DB_DATABASE || 'localhost',
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DATABASE,
 })
 
 connection.connect((err) => {
