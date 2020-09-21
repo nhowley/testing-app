035ddc6fe0a75befed062dfc5cb346909d552f1f
Natalie <natalie@emeraldstay.com>
2020-09-21 16:14:09 +0200

    add database

--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
-// 'user strict'
+'user strict'
 
-// const mysql = require('mysql')
-// require('dotenv').config()
+const mysql = require('mysql')
+require('dotenv').config()
 
-// const connection = mysql.createConnection({
-//   host     : process.env.DB_HOST || 'localhost',
-//   user     : process.env.DB_USER || 'localhost',
-//   password : process.env.DB_PASS || 'localhost',
-//   database : process.env.DB_DATABASE || 'localhost',
-// })
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST || 'localhost',
+  user     : process.env.DB_USER || 'localhost',
+  password : process.env.DB_PASS || 'localhost',
+  database : process.env.DB_DATABASE || 'localhost',
+})
 
-// connection.connect((err) => {
-//   if (err) {
-//     throw err
-//   }
-// })
+connection.connect((err) => {
+  if (err) {
+    throw err
+  }
+})
 
-// module.exports = connection
+module.exports = connection
--- routes/db.js
+++ routes/db.js
@@ -1,23 +1,23 @@
-// const db = require('../db/db');
+const db = require('../db/db');
 
-// // return all cats in database
-//     const cats = (app) => {
-//         app.route('/cats').get(async (req, res) => {
-//             console.log("cats reached")
-//             let query = 'SELECT * FROM cats' 
-//             const results = await new Promise((resolve, reject) => db.query(query, (err, Qresults) => {
-//                 if (err) {
-//                     reject(err);
-//                 } else {
-//                     resolve(Qresults);
-//                 }
-//             }
-//             ));
-//             console.log("results", results)
-//             res.json(results)
-//     })
-// };
+// return all cats in database
+    const cats = (app) => {
+        app.route('/cats').get(async (req, res) => {
+            console.log("cats reached")
+            let query = 'SELECT * FROM cats' 
+            const results = await new Promise((resolve, reject) => db.query(query, (err, Qresults) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(Qresults);
+                }
+            }
+            ));
+            console.log("results", results)
+            res.json(results)
+    })
+};
 
-// module.exports = function routes (app) {
-//     cats(app)
-//   }
+module.exports = function routes (app) {
+    cats(app)
+  }
