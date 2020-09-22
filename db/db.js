6ec86dfc15272c3f8cbc5e307239518c86ea7a25
Natalie <natalie@emeraldstay.com>
2020-09-22 22:51:00 +0200

    add new dataabse connection

--- db/db.js
+++ db/db.js
@@ -1,31 +1,38 @@
 'user strict'
 
-const mysql = require('mysql');
-const { env } = require('process');
+const mysql = require('mysql')
 require('dotenv').config()
-const { Pool, Client } = require('pg')
 
-// const connection = mysql.createConnection({
-//   host     : 'gnldm1014.siteground.biz',
-//   user     : 'umtnndmjmcezv',
-//   password : process.env.DB_PASS,
-//   database : 'db6hextktaa2yb',
+// // const connection = mysql.createConnection({
+// //   host     : 'gnldm1014.siteground.biz',
+// //   user     : 'umtnndmjmcezv',
+// //   password : process.env.DB_PASS,
+// //   database : 'db6hextktaa2yb',
+// // })
+
+
+// const connection = mysql.createConnection(`${process.env.DATABASE_URL}`);
+
+// connection.connect((err) => {
+//   if (err) {
+//     throw err
+//   }
 // })
 
-const connection = mysql.createConnection(`${process.env.DATABASE_URL}`);
+// module.exports = connection
 
-// const connection = new Pool({
-//   user: 'wkjrrgatzzehmi',
-//   host: 'ec2-54-228-209-117.eu-west-1.compute.amazonaws.com',
-//   database: 'd98k2uh7gfp8f5',
-//   password: '0d1b97da4e0ae5fd9bd13327843717cfe1165cb29592817faf5fd1ef13d532f8',
-//   port: 5432,
-// })
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : DB_HOST,
+  user            : DB_USER,
+  password        : DB_PASS,
+  database        : DB_DATABASE
+});
 
-connection.connect((err) => {
-  if (err) {
-    throw err
-  }
-})
+// pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
 
-module.exports = connection
+
+module.exports = pool
