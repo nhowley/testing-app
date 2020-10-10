535ef6c85c405d050a3ef43273883d80f76680ea
Natalie <natalie@emeraldstay.com>
2020-10-10 19:38:17 +0200

    filter reports by date

--- react-reports/src/App.js
+++ react-reports/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios"
 import json from "../../public/reports/daily.json";
 import DayPicker from './Components/DayPicker'
+import moment from 'moment'
 
 class App extends Component {
     constructor(props) {
@@ -21,35 +22,54 @@ class App extends Component {
         })
     }
 
-    getClientResults = (e) => {
-        console.log("time to get some results for", this.state.email)
-        let email = this.state.email
+    getClientResults = async (e) => {
+        let allClientReports = await this.getClientReportsByEmail()
+        let clientReportsInRange = await this.getClientReportsInRange(allClientReports)
+        
+         // let datesArray = this.datesArray(newdataArr[email])
 
+        this.setState({
+            allClientReports: allClientReports,
+            clientReportsInRange: clientReportsInRange
+            // datesArray: datesArray
+        })
+
+    }
+
+    getClientReportsByEmail = () => {
+        let email = this.state.email
         let dataArr = Object.entries(this.state.JSONResults)
-        console.log("dataArr", dataArr)
         let newdataArr = {}
         let data = {}
-        // let data = []
+
         dataArr.forEach(([key, value], index) => {
-            console.log("key", value)
             
             Object.entries(value).forEach(([key2, value2]) => {
-                console.log("key2", key2)
                 if (key2 === email){
                     data[index] = value2
                     newdataArr[key2] = data
                 }
                 console.log("data", data)
-                // data.push(value2)
             })
         });
-        console.log("newDataArr", newdataArr)
-        let datesArray = this.datesArray(newdataArr[email])
-        console.log("datesArray", datesArray)
-        this.setState({
-            clientReports: newdataArr,
-            datesArray: datesArray
+
+        return newdataArr
+    }
+
+    // gets all reports for client in date range selected
+    getClientReportsInRange = (allClientReports) => {
+        let reports = Object.values(allClientReports)
+        console.log("reports", Object.values(reports))
+        
+        let reportsInDateRange = []
+
+        Object.entries(reports).forEach(([number, report]) => {
+           let reportArr = Object.values(report)
+           reportsInDateRange = reportArr.filter(element => this.state.datesSelected.includes(moment(element).format("YYYY-MM-DD")));
+            console.log("reportsInDateRange", reportsInDateRange)
         })
+
+        return reportsInDateRange
     }
 
     datesArray = (data) => {
@@ -62,6 +82,36 @@ class App extends Component {
         return dates
     }
 
+    setDatesSelected = (from, to) => {
+        if (from !== undefined && to !== undefined) {
+            let datesSelected = this.getDateArray(from, to)
+            this.setState({
+                from: from,
+                to: to,
+                datesSelected: datesSelected
+            })
+        } else {
+            this.setState({
+                from: from,
+                to: to
+            })
+        }
+        
+    }
+
+    getDateArray = (start, end) => {
+        var
+          arr = new Array(),
+          dt = new Date(start);
+  
+        while (dt <= end) {
+          arr.push(moment(dt).format('YYYY-MM-DD'));
+          dt.setDate(dt.getDate() + 1);
+        }
+        return arr;
+      }
+  
+
     render(){
 
         return(
@@ -69,7 +119,7 @@ class App extends Component {
                 <label htmlFor="email">Client email</label>
                 <br/>
                <input type="email" onChange={(e) => {this.updateEmail(e)}} id="email"></input>
-               <DayPicker />
+               <DayPicker setDatesSelected={this.setDatesSelected}/>
                <button onClick={() => {this.getClientResults()}}>Submit</button>
             </div>
         );
--- react-reports/src/Components/DayPicker.js
+++ react-reports/src/Components/DayPicker.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet'; // uninstall if not used later
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-export default class Example extends React.Component {
+export default class Calendar extends React.Component {
   static defaultProps = {
     numberOfMonths: 2,
   };
@@ -22,6 +22,13 @@ export default class Example extends React.Component {
     };
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.from !== this.state.from || prevState.to !== this.state.to){
+        console.log("CHANGE IN STATE")
+        this.props.setDatesSelected(this.state.from, this.state.to)
+        }
+    }
+
   handleDayClick(day) {
     const range = DateUtils.addDayToRange(day, this.state);
     this.setState(range);
@@ -35,7 +42,7 @@ export default class Example extends React.Component {
     const { from, to } = this.state;
     const modifiers = { start: from, end: to };
     return (
-      <div className="RangeExample">
+      <div className="RangeExample mt-4">
         <p>
           {!from && !to && 'Please select the first day.'}
           {from && !to && 'Please select the last day.'}
@@ -56,25 +63,6 @@ export default class Example extends React.Component {
           modifiers={modifiers}
           onDayClick={this.handleDayClick}
         />
-        <Helmet>
-          <style>{`
-  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
-    background-color: #f0f8ff !important;
-    color: #4a90e2;
-  }
-  .Selectable .DayPicker-Day {
-    border-radius: 0 !important;
-  }
-  .Selectable .DayPicker-Day--start {
-    border-top-left-radius: 50% !important;
-    border-bottom-left-radius: 50% !important;
-  }
-  .Selectable .DayPicker-Day--end {
-    border-top-right-radius: 50% !important;
-    border-bottom-right-radius: 50% !important;
-  }
-`}</style>
-        </Helmet>
       </div>
     );
   }
